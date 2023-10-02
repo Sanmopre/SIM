fadbfbac21a0633bce6c1817fc96abd96384a2f0
Sanmopre <moliner.santiago@gmail.com>
2023-10-02 20:19:09 +0200

    Initial commit

---
+++ build/CMakeFiles/box_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/box_test.dir/src/main.cpp.o"
+  "CMakeFiles/box_test.dir/src/main.cpp.o.d"
+  "CMakeFiles/box_test.dir/src/shaderManager.cpp.o"
+  "CMakeFiles/box_test.dir/src/shaderManager.cpp.o.d"
+  "box_test"
+  "box_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/box_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
