9334fcf6f67326271843d5ccbcdd152217885fc9
Santiago Moliner <40120264+Sanmopre@users.noreply.github.com>
2023-11-05 16:16:07 +0100

    Added camera and model loading

--- src/render.cpp
+++ src/render.cpp
@@ -1,6 +1,5 @@
 #include "render.h"
 #include <iostream>
-#include "raylib.h"
 
 Render::Render(std::string name) : Module(name) {
 
@@ -10,47 +9,77 @@ Render::~Render() {
 
 }
 
-bool Render::LoadConfig(std::string config_file) {
+bool Render::LoadConfig(std::string config_file) 
+{
     return true;
 }
 
-bool Render::Start() {
-    
-    // Initialize the window
-    InitWindow(800, 600, "My Window");
+bool Render::Start() 
+{
+    camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };  // Camera position
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
+
+    Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
+
+    InitWindow(screenWidth, screenHeight, windowTitle.c_str());
+    SetTargetFPS(targetFPS);
+
+    model = LoadModel("models/cottage_obj.obj");                    // Load the animated model mesh and basic data
+    texture = LoadTexture("models/cottage_textures/cottage_diffuse.png");  
+    texture2 = LoadTexture("models/cottage_textures/cottage_normal.png");        // Load model texture and set material
+    SetMaterialTexture(&model.materials[0], MATERIAL_MAP_DIFFUSE, texture); 
+    SetMaterialTexture(&model.materials[1], MATERIAL_MAP_NORMAL, texture2); 
     return true;
 }
 
-bool Render::Update(double delta_time) {
+bool Render::Update(double delta_time) 
+{
+    if(IsKeyDown(KEY_ESCAPE))
+        return false;
 
-    // Set the font size and color
-    int fontSize = 20;
-    Color fontColor = BLACK;
 
-    // Set the text to display
-    const char* text = "Hello, world!";
 
-    // Calculate the text position
-    Vector2 textSize = MeasureTextEx(GetFontDefault(), text, fontSize, 1);
-    Vector2 textPosition = {(float)(GetScreenWidth() - textSize.x) / 2, (float)(GetScreenHeight() - textSize.y) / 2};
+    angle.y = 0.0f;
+    angle.x = 0.0f;
 
-    // Begin drawing
+    if(IsKeyDown(KEY_UP)) 
+        angle.y = -rotationSpeed * delta_time;
+    if(IsKeyDown(KEY_DOWN)) 
+        angle.y = rotationSpeed * delta_time; 
+    if(IsKeyDown(KEY_LEFT)) 
+        angle.x = -rotationSpeed * delta_time;
+    if(IsKeyDown(KEY_RIGHT)) 
+        angle.x = rotationSpeed * delta_time; 
+
+    float currentSpeed = 0.0f;
+    if(IsMouseButtonDown(MOUSE_LEFT_BUTTON)) 
+        currentSpeed = speed * delta_time;
+    if(IsMouseButtonDown(MOUSE_RIGHT_BUTTON))   
+        currentSpeed = -speed * delta_time;
+
+    //camera.up = (Vector3){ 0.0f, angle, 0.0f };  
+    UpdateCameraPro(&camera, (Vector3){ currentSpeed, 0.0f, 0.0f }, (Vector3){ angle.x, angle.y, 0.0f }, 0.0f);  // Update camera
+
+
+
     BeginDrawing();
+    ClearBackground(RAYWHITE);
+    
+        BeginMode3D(camera);
 
-    // Draw the text
-    DrawText(text, textPosition.x, textPosition.y, fontSize, fontColor);
+        DrawModelEx(model, (Vector3){ 0.0f, 0.0f, 0.0f }, (Vector3){ 1.0f, 0.0f, 0.0f }, 0.0f, (Vector3){ 0.20f, 0.20f, 0.20f }, WHITE);
+        DrawGrid(10, 1.0f);
 
-    // End drawing
+        EndMode3D();
     EndDrawing();
 
-    // Close the window when the user presses the escape key
-    if (IsKeyPressed(KEY_ESCAPE)) {
-        return false;
-    }
-
     return true;
 }
 
-void Render::Cleanup() {
-
+void Render::Cleanup() 
+{
+    CloseWindow();
 }
--- src/render.h
+++ src/render.h
@@ -1,4 +1,6 @@
 #include "module.h"
+#include "raylib.h"
+#include <string>
 
 class Render : public Module {
 public:
@@ -8,4 +10,18 @@ public:
     virtual bool Start() override;
     virtual bool Update(double delta_time) override;
     virtual void Cleanup() override;
+
+private:
+    const int screenWidth = 700;
+    const int screenHeight = 700;
+    const int targetFPS = 60;
+    const float rotationSpeed = 30.0f;
+    const float speed = 10.0f;
+    std::string windowTitle = "SIM";
+    Model model;                 
+    Texture2D texture;
+    Texture2D texture2;
+    Camera3D camera;
+
+    Vector2 angle = { 0.0f, 0.0f };
 };
